41018ea7f994325c7069b09b0182448a5ba906f2
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-12-15 20:02:39 -0300

    feature/new-recommendation/category-option

--- frontend/src/components/RecomendModal/index.jsx
+++ frontend/src/components/RecomendModal/index.jsx
@@ -1,4 +1,4 @@
-import { Button, Modal, Form } from 'react-bootstrap';
+import { Button, Modal, Form, Row, Col } from 'react-bootstrap';
 
 export default function AskModal({ show, handleClose }) {
     return (
@@ -12,6 +12,36 @@ export default function AskModal({ show, handleClose }) {
             </Modal.Header>
             <Modal.Body>
                 <Form>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Categoy/Subcategory:</Form.Label>
+                        <Row>
+                            <Col>
+                                <Form.Select aria-label="Default select example">
+                                    <option>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </Form.Select>
+                            </Col>
+                            <Col>
+                                <Form.Select aria-label="Default select example" disabled={false}>
+                                    <option>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </Form.Select>
+                            </Col>
+                        </Row>
+                        <Row className='mt-3'>
+                            <Col>
+                                <Form.Check>
+                                    <Form.Check.Input type="checkbox" />
+                                    <Form.Check.Label>New subcategory</Form.Check.Label>
+                                </Form.Check>
+                                <Form.Control type="text" autoComplete='off' maxlength="15" disabled={true} />
+                            </Col>
+                        </Row>
+                    </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
                         <Form.Label>Youtube URL: </Form.Label>
                         <Form.Control type="text" placeholder="https://www.youtube.com/watch?..." autoComplete='off' />
